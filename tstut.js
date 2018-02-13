1a589a5faf25a071398e1edd359ca80531ec609f
Lakshman Karunasena <lakkaru@gmail.com>
2018-02-13 11:00:28 +0530

    Let and Var

--- tstut.js
+++ tstut.js
@@ -25,3 +25,13 @@ superHeros.push({
     superName: "lakkaru"
 });
 document.write(superHeros[0].realName + " is " + superHeros[0].superName + "<br/>");
+var sampleLet = 123;
+if (true) {
+    var sampleLet_1 = 456;
+}
+document.write("sampleLet " + sampleLet + "<br/>");
+var sampleVar = 123;
+if (true) {
+    var sampleVar = 456;
+}
+document.write("sampleVar " + sampleVar + "<br/>");
--- tstut.ts
+++ tstut.ts
@@ -37,3 +37,15 @@ superHeros.push({
     superName:"lakkaru"
 });
 document.write(superHeros[0].realName+" is "+superHeros[0].superName+"<br/>");
+
+let sampleLet=123;
+if(true){
+    let sampleLet=456;
+}
+document.write("sampleLet " +sampleLet+"<br/>");
+
+var sampleVar=123;
+if(true){
+    var sampleVar=456;
+}
+document.write("sampleVar " +sampleVar+"<br/>");
